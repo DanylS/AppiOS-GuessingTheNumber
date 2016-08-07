869ca54e2d27d45cd8bad71e46f0435a3f1cbf72
Danyl Semmache <Danyl@danyls-macbook-pro.local>
2016-08-07 18:10:06 +0200

    Initial Commit

---
+++ Guessing the number/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guessing the number
+//
+//  Created by Danyl Semmache on 07/08/16.
+//  Copyright © 2016 Danyl Semmache. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
